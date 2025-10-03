091c2f1e952f2e0c6bfecc4312e401562113e482
manojthapa <1995msthapa@gmail.com>
2025-10-03 19:36:41 +0530

    [AppList] implemented safe-nav args

--- app/src/main/java/com/example/applist/view/AppDetailFragment.kt
+++ app/src/main/java/com/example/applist/view/AppDetailFragment.kt
@@ -5,9 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.applist.R
 
 class AppDetailFragment : Fragment() {
+    private val args: AppDetailFragmentArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +22,14 @@ class AppDetailFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_app_detail, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val name = args.appName
+        val image = args.appImage
+        val release = args.releaseDate
+        val playStoreUrl = args.playStoreUrl
     }
 
 }
