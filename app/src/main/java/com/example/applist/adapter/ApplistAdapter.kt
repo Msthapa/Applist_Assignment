6ab6d26f23170730b97be04e3ff4605c8bb95de0
manojthapa <1995msthapa@gmail.com>
2025-10-03 11:55:01 +0530

    [AppList] initial commit with 1. Launcher Activity 2. Applist fragment 3. Viewbinding enabled

---
+++ app/src/main/java/com/example/applist/adapter/ApplistAdapter.kt
@@ -0,0 +1,23 @@
+package com.example.applist.adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ApplistAdapter: RecyclerView.Adapter<ApplistAdapter.ApplistViewholder>() {
+
+    inner class ApplistViewholder(): RecyclerView.ViewHolder() {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ApplistViewholder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ApplistViewholder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
